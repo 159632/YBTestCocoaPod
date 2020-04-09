fb9398ca00ff5d7ef458801e7281c231c795d1b3
渊博 <yuanbo@Mac-mini.local>
2020-04-09 11:14:29 +0800

    添加pod文件

---
+++ YBTestCocoapod/YBtestCocoapodViewController.swift
@@ -0,0 +1,31 @@
+//
+//  YBtestCocoapodViewController.swift
+//  YBTestCocoapod
+//
+//  Created by 渊博 on 2020/4/9.
+//  Copyright © 2020 渊博. All rights reserved.
+//
+
+import UIKit
+
+class YBtestCocoapodViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         
+        // Do any additional setup after loading the view.
+      print("1231231232132131231")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
